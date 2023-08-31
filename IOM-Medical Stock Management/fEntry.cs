fb132e8959799a32973cd5f0eafccda4dac84b81
Cem Savaş Kaya <cskaya@aof.anadolu.edu.tr>
2023-08-31 14:28:36 +0300

    Add project files.

---
+++ IOM-Medical Stock Management/fEntry.cs
@@ -0,0 +1,10 @@
+namespace IOM_Medical_Stock_Management
+{
+    public partial class fEntry : Form
+    {
+        public fEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
